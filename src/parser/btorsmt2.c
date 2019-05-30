30ba1605cf35af899cf937aefb90e7392247733d
Mathias Preiner <mathias.preiner@gmail.com>
2019-05-29 19:52:36 -0700

    Fix model printing for Bool variables.
    Additionally store information on Boolean variable to print them
    accordingly in SMT2 models.

--- src/parser/btorsmt2.c
+++ src/parser/btorsmt2.c
@@ -30,6 +30,7 @@ BTOR_DECLARE_STACK (BoolectorNodePtr, BoolectorNode *);
 /*------------------------------------------------------------------------*/
 
 void boolector_print_value_smt2 (Btor *, BoolectorNode *, char *, FILE *);
+void boolector_var_mark_bool (Btor *, BoolectorNode *);
 
 /*------------------------------------------------------------------------*/
 
@@ -3880,6 +3881,7 @@ declare_fun_smt2 (BtorSMT2Parser *parser, bool isconst)
 {
   uint32_t i;
   int32_t tag;
+  bool is_bool_var = false;
   BoolectorSortStack args;
   BtorSMT2Node *fun;
   fun = 0;
@@ -3930,6 +3932,7 @@ declare_fun_smt2 (BtorSMT2Parser *parser, bool isconst)
 
   /* parse return sort */
   tag = read_token_smt2 (parser);
+  is_bool_var = tag == BTOR_BOOL_TAG_SMT2;
   if (!parse_sort (parser, tag, true, &sort))
   {
     BTOR_RELEASE_STACK (args);
@@ -3952,6 +3955,7 @@ declare_fun_smt2 (BtorSMT2Parser *parser, bool isconst)
     else
     {
       fun->exp = boolector_var (parser->btor, sort, fun->name);
+      if (is_bool_var) boolector_var_mark_bool (parser->btor, fun->exp);
       BTOR_MSG (boolector_get_btor_msg (parser->btor),
                 2,
                 "declared '%s' as bit-vector at line %d column %d",
