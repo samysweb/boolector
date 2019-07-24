1989080261235f33e344cbd095e70a337c45bd16
Mathias Preiner <mathias.preiner@gmail.com>
2019-07-24 15:38:41 -0700

    btorcadical: Fix deref to comply with new CaDiCaL API changes.

--- src/sat/btorcadical.c
+++ src/sat/btorcadical.c
@@ -45,7 +45,13 @@ assume (BtorSATMgr *smgr, int32_t lit)
 static int32_t
 deref (BtorSATMgr *smgr, int32_t lit)
 {
-  return ccadical_deref (smgr->solver, lit);
+  int32_t val;
+  val = ccadical_deref (smgr->solver, lit);
+  if (val > 0)
+    return 1;
+  if (val < 0)
+    return -1;
+  return 0;
 }
 
 static void
